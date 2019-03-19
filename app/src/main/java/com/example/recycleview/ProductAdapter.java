372ae7c0b0c7feb121c97b6c7cdd602434ef3c31
thaani <38487289+thaani@users.noreply.github.com>
2019-03-19 04:17:32 -0700

    Initial commit

---
+++ app/src/main/java/com/example/recycleview/ProductAdapter.java
@@ -0,0 +1,68 @@
+package com.example.recycleview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+    private Context mCtx;
+
+    //we are storing all the products in a list
+    private List<Product> productList;
+
+    public ProductAdapter(Context mCtx, List<Product> productList) {
+        this.mCtx = mCtx;
+        this.productList = productList;
+    }
+
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //inflating and returning our view holder
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.layout_products, null);
+        return new ProductViewHolder(view);
+    }
+
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        //getting the product of the specified position
+        Product product = productList.get(position);
+
+        //binding the data with the viewholder views
+        holder.textViewTitle.setText(product.getTitle());
+        holder.textViewShortDesc.setText(product.getShortdesc());
+        holder.textViewRating.setText(String.valueOf(product.getRating()));
+        holder.textViewPrice.setText(String.valueOf(product.getPrice()));
+
+        holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(product.getImage()));
+
+    }
+
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewTitle, textViewShortDesc, textViewRating, textViewPrice;
+        ImageView imageView;
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewShortDesc = itemView.findViewById(R.id.textViewShortDesc);
+            textViewRating = itemView.findViewById(R.id.textViewRating);
+            textViewPrice = itemView.findViewById(R.id.textViewPrice);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
+
+
+//getting the context and product list with constructor
+
